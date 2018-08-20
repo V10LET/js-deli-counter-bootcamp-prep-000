7616ceb5c8710abcfe4be85e9de59877fe9e1201
Violet Moon <violet.wmoon@gmail.com>
2018-08-20 19:01:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 function takeANumber(currentLine, newPersonsName) {
-  return `Welcome, ${newPersonsName}. You are number ${currentLine.length} in line.`
+  return `Welcome, ${newPersonsName}. You are number ${currentLine.length+1} in line.`
 }
