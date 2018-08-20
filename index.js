c54f2cfee9cea4f0c4ba24c94d3e8e83a7111d77
Violet Moon <violet.wmoon@gmail.com>
2018-08-20 19:20:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,13 @@ function nowServing(katzDeliLine) {
     return "Currently serving " + currentPerson + "."
   }
 }
+
+function currentLine(line) {
+  if (line.length === 0) {
+    return `The line is currently empty.`
+  } else {
+    
+
+  }
+}
+return `The line is currently ${}`
