2c336d0920fd27f693c77030659850be47eb4226
Violet Moon <violet.wmoon@gmail.com>
2018-08-20 19:07:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ function takeANumber(currentLine, newPersonsName) {
   currentLine.push(newPersonsName)
   return `Welcome, ${newPersonsName}. You are number ${currentLine.length} in line.`
 }
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    `There is nobody waiting in line.`
+  } else {
+    
+  }
+}
